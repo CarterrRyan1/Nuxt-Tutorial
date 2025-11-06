ce934e92fa9db873aa104128c737e62aac3043e0
CarterrRyan <carter.ryan.todd@kainos.com>
2025-11-06 22:37:08 +0000

    ✅ ask 6: Composables (Reusable Logic)

--- app/components/counter.vue
+++ app/components/counter.vue
@@ -1,21 +1,15 @@
 <template>
         <div class="border border-solid border-black p-2 m-2 rounded-sm">
             <h1>Counter Component</h1>
-            <p>{{ counter || 0 }}</p>
+            <p>{{ count || 0 }}</p>
             <div>
-                <button @click="add" class="rounded-sm bg-green-800 hover:bg-green-700 p-1 m-1 text-white">Add</button>
-                <button @click="subtract"
-                    class="rounded-sm bg-red-800 hover:bg-red-700 p-1 m-1 text-white">Subtract</button>
+                <button @click="increment" class="rounded-sm bg-green-800 hover:bg-green-700 p-1 m-1 text-white">Add</button>
+                <button @click="decrement" class="rounded-sm bg-red-800 hover:bg-red-700 p-1 m-1 text-white">Subtract</button>
+                <button @click="reset" class="rounded-sm bg-gray-800 hover:bg-gray-700 p-1 m-1 text-white">Reset</button>
             </div>
         </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-const counter = ref<number>(0);
-const add = (): void => {
-    counter.value++;
-}
-const subtract = () => {
-    counter.value--;
-}
+import { useCounter } from '../composables/useCounter';
+const {count,increment,decrement,reset}=useCounter(10);
 </script>
